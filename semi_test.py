b2aa4b905e2f337f4ce3add8048fcd61b2b40236
ZenTheDarkGod <takaczsoli@gmail.com>
2023-12-30 17:46:28 +0100

    Chore | updated upload details

--- semi_test.py
+++ semi_test.py
@@ -3,17 +3,7 @@ import src.zenyx as tzenyx
 import tests.databank as db
 
 def test_one():
-    
-    c: list = []
-    
-    tzenyx.pyon.debug.init(__file__)
-    
-    x = tzenyx.pyon.deep_serialize(db.demo_obj)
-    y = tzenyx.pyon.deep_parse(x)
-    
-    z = tzenyx.pyon.deep_serialize(y)
-    
-    print(f"Z: {z}\nZ as JSON: {tzenyx.pyon.dumps(z)}")
+    pass
     
 
 test_one()
