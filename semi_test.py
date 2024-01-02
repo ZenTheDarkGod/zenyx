1aa42165971b0da0e787d475f2d41f6da102d98b
ZenTheDarkGod <takaczsoli@gmail.com>
2024-01-02 23:20:59 +0100

    1.2.0 | more title options

--- semi_test.py
+++ semi_test.py
@@ -3,8 +3,8 @@ from zenyx import printf
 
 def test_one():
     printf.clear_screen()
-    printf.print_full_height()
-    printf.title("@!Bold ass$& / bitches")
+    printf.full()
+    printf.title("@!Bold ass$& / bitches", " ")
     printf.full_line("asd", "asd", sep="-")
     
 
--- src/zenyx/console.py
+++ src/zenyx/console.py
@@ -53,19 +53,22 @@ class printf:
 
         printf(f'{printf.__filter(content)}{" "*(terminal_width - len(printf.__rm_filter(content)))}', **kwargs)
 
-    def print_full_height() -> None:
+    def full() -> None:
         for i in range(os.get_terminal_size().lines - 1):
             printf.full_line(end="\n")
 
     def endl(times: int = 1):
         print("\n" * times, end="")
 
-    def title(content: str) -> None:
+    def title(content: str, line_char: chr = "─") -> None:
         width: int = os.get_terminal_size().columns
         mid_text: str = f" {content} "
         side_width: int = int((width - len(printf.__rm_filter(mid_text))) / 2)
 
-        sep_text = "─" * side_width
+        if len(line_char) != 1:
+            raise ValueError("line char is not a char")
+
+        sep_text = line_char * side_width
         printf.endl(5)
         printf(f"{sep_text}{mid_text}{sep_text}")
         printf.endl()
