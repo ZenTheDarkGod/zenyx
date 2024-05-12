8093c60fa241fa9597002b4fe89562bf37a343bc
unknown <takaczsoli@gmail.com>
2024-05-12 12:56:17 +0200

    tests updated

--- semi_test.py
+++ semi_test.py
@@ -3,13 +3,58 @@ import src.zenyx as zenyx
 import sys
 import tests.databank as db
 
+
+from result import *
+
+def unit(u: str) -> float:
+    """
+    ## Units
+    x   - pixel\n
+    h   - screen height\n
+    w   - screen width\n
+    u   - 16px \n
+
+    ## Example
+    ```
+    el(
+        style=style(
+            x="12u",
+            y="14w"
+        )
+    )
+    ```
+    """
+
+    # num_res = float(u[:-1])
+
+    # if num_res.is_err():
+    #     return 0
+
+    num: float = float(u[:-1])
+
+    match u[-1]:
+        case "x":
+            return num
+
+        case "h":
+            return 1600 * (num / 100)
+
+        case "w":
+            return 1600 * (num / 100)
+
+        case "u":
+            return num * 16
+
+def err():
+    raise Exception("asdasd")
+
 def test_one():
-    zenyx.pyon.debug.init(__file__)
-    myobj = zenyx.pyon.deep_parse(zenyx.pyon.deep_serialize(db.demo_obj))
-    print(myobj)
-    myobj.param = 'asd'
-    print(myobj)
-    print((zenyx.pyon.deep_serialize(myobj)))
+    print(unit('50x'))
+
+    x = zenyx.std.attempt(err, [])
+
+    if x.is_err():
+        print(x.err())
 
 
 test_one()
