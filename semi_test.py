1df57e878ff38c35b467717efc8db07f20a517f6
zewenn <takaczsoli@gmail.com>
2024-06-19 13:00:28 +0200

    added more std fns

--- semi_test.py
+++ semi_test.py
@@ -1,69 +1,11 @@
+from dataclasses import dataclass
 import src.zenyx as zenyx
-import sys
-import tests.databank as db
+import math
 
+@zenyx.struct
+class Vec2:
+    x: int = 0
+    y: int = 0
 
-from result import *
-
-
-def unit(u: str) -> float:
-    """
-    ## Units
-    x   - pixel\n
-    h   - screen height\n
-    w   - screen width\n
-    u   - 16px \n
-
-    ## Example
-    ```
-    el(
-        style=style(
-            x="12u",
-            y="14w"
-        )
-    )
-    ```
-    """
-
-    # num_res = float(u[:-1])
-
-    # if num_res.is_err():
-    #     return 0
-
-    num: float = float(u[:-1])
-
-    match u[-1]:
-        case "x":
-            return num
-
-        case "h":
-            return 1600 * (num / 100)
-
-        case "w":
-            return 1600 * (num / 100)
-
-        case "u":
-            return num * 16
-
-
-def err():
-    raise Exception("asdasd")
-
-
-def transf(lst: list[int]) -> list[str]:
-    return [str(x) for x in lst]
-
-
-def filter(lst: list[str]) -> list[str]:
-    return [x for x in lst if x == "10"]
-
-
-def test_one():
-    x = zenyx.Pipe([10, 20, 10])
-    x >> transf >> filter >> " ".join
-    x = x()
-
-    print(x)
-
-
-test_one()
+    def add(self: "Vec2") -> int:
+        return self.x + self.y
--- src/zenyx/__init__.py
+++ src/zenyx/__init__.py
@@ -28,7 +28,72 @@ Includes the reset (`$&`) symbol \n
 - `printf.title(<content>, <line_char>)` -> Prints the title to the middle of the console using the line_char as the spacing chartacter
 """
 
-from zenyx import pyon, require, streams
-from zenyx.console import printf
-from zenyx.args import Arguments
-from zenyx.pipe import Pipe
+from dataclasses import dataclass
+from typing import *
+import inspect
+import copy
+import sys
+
+from . import pyon, require, streams
+from .console import printf
+from .args import Arguments
+from .pipe import Pipe
+
+
+T = TypeVar("T")
+K = TypeVar("K")
+
+
+def unreachable(msg: str) -> Never:
+    stack = inspect.stack()[1]
+    printf(
+        "\n\n@!unreachable$&"
+        + f"\n@~In {stack.filename}, line {stack.lineno} in {stack.function}()$&"
+    )
+    printf(f"\n@!Error Message:$&\n{msg}")
+    raise Exception("unreachable")
+
+
+class DummyFile:
+    def write(self, x):
+        pass
+
+    def flush(self, x=None):
+        pass
+
+
+def silence(func: Callable):
+    def wrap(*args, **kwargs):
+        res, exc = None, None
+        save_stdout = sys.stdout
+        sys.stdout = DummyFile()
+        try:
+            res = func(*args, **kwargs)
+        except Exception as e:
+            exc = e
+        sys.stdout = save_stdout
+        if exc:
+            raise exc
+        return res
+
+    return wrap
+
+
+def struct(cls: object.__class__):
+    for attr, Type in inspect.getmembers(cls):
+        if Type.__class__.__name__ == "function":
+            delattr(cls, attr)
+
+    return dataclass(cls)
+
+
+def clone(a: T) -> T:
+    return copy.copy(a)
+
+
+def structured_clone(a: T) -> T:
+    return copy.deepcopy(a)
+
+
+def weak_cast(Type: T, a: Any) -> T:
+    return a
